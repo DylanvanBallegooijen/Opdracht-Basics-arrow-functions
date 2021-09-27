2a9174c2766d237af9371af86a5c49e6e7f37182
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-09-27 09:59:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// zet de functie om naar een arrow functie
+/*const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions()*/
+
+//let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+//ikRockArrowFunctions()
+
+/*const fivePlusSeven = function () {
+    return 5 + 7
+};
+
+fivePlusSeven();*/
+
+
+let Myfunction = () => 5 + 7
+Myfunction();
+
